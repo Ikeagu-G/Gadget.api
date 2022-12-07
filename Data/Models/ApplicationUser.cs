a05c15cff66a2b1a1af9bd0634e7677097883fb2
erivics <adesinakbayo@gmail.com>
2022-12-07 09:16:11 +0100

    application user model modified

--- Data/Models/ApplicationUser.cs
+++ Data/Models/ApplicationUser.cs
@@ -3,16 +3,30 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Gadget.api.Data.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser, IBaseEntity
     {
-        public string? Password { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? MiddleName { get; set; }
+        public string Password { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
         public Status Status { get; set; }
-        public string? CreatedById { get; set; }
-        public string? CreatedBy { get; set;}
+        public string CreatedById { get; set; } = string.Empty;
+        public string CreatedBy { get; set; }  = string.Empty;
         public DateTime DateCreated { get; set; }
+    }
+
+    public class BaseEntity : IBaseEntity
+    {
+        public string CreatedById { get; set; } = string.Empty;
+        public string CreatedBy { get; set;} = string.Empty;
+        public DateTime DateCreated { get; set; }
+    }
+
+    public interface IBaseEntity
+    {
+        string CreatedById { get; set; }
+        string CreatedBy { get; set;}
+        DateTime DateCreated { get; set; }
     }
 
 }
